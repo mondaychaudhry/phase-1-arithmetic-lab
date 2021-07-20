189cf6cc7c11b307b70edcc28c8aa0d737fc700c
Monday <sobaata@gmail.com>
2021-07-20 09:13:23 -0400

    Completed.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+
+let random = Math.ceil(Math.random())
+
+let num3 = 8
+let num4 = 2
+const mod = num3 / num4 
+
+
+const max = Math.max(4,5,20)
